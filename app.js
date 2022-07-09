44177119f30f0eea51e8bb15f7003fe751aa5281
Ålexander <yurchenko@post.com>
2022-07-09 16:39:48 +0200

    Fist pattern is finished

---
+++ app.js
@@ -0,0 +1,9 @@
+const rd_01 = document.getElementsByClassName("rd_first");
+const rd_02 = document.getElementsByClassName("rd_second");
+const rd_03 = document.getElementsByClassName("rd_third");
+const rd_04 = document.getElementsByClassName("rd_fourth");
+
+console.log(rd_01);
+console.log(rd_02);
+console.log(rd_03);
+console.log(rd_04);
